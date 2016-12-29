ceb6c103a34c098fa314c9976d404adf214429e1
Rafay Hasan <rafayhasan@rafays-macbook-pro-2.local>
2016-12-29 23:35:37 +0600

    Fixing- Menu admin and menu user page auto layout issue.

---
+++ StudioApp Readytec/TrainingDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TrainingDetailsViewController.h
+//  StudioApp Readytec
+//
+//  Created by Rafay Hasan on 12/28/16.
+//  Copyright © 2016 Rafay Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrainingDetailsViewController : UIViewController
+
+@end
